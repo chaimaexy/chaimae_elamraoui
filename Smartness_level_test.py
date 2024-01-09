4404aa45f7d75c9ac19544401b9c0ada7fbed8c6
Nihad El Bari <110766469+Niihaad@users.noreply.github.com>
2024-01-09 02:46:31 +0100

    Add files via upload
    Python file

---
+++ Smartness_level_test.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  8 23:32:32 2024
+
+@author: nihqd
+"""
+
+import numpy as np
+import pickle 
+import streamlit as st
+
+loaded_model = pickle.load(open('D:\\FirstDeployment\\trained_modelIntell.sav', 'rb'))
+
+
+def smartness_prediction(input_data):
+    # Example of input_data = [[0.9, 0.7, 0.1]]
+    predicted_outcome = loaded_model.predict(input_data)
+    return 'You are ' + str(predicted_outcome[0])  # Convert the prediction to a string
+
+
+def main():
+    st.title('Test Your Level of Smartness By Nihad ')
+    
+    usingGpt = st.text_input('On a scale from 0 to 1, how frequently do you use ChatGPT ?')
+    usingGoogle = st.text_input('On a scale from 0 to 1, how often do you rely on Google ?')
+    usingMind = st.text_input('On a scale from 0 to 1, how frequently do you engage your mind ?')
+    
+    total = ''
+    if st.button("Predict Smartness"):
+        try:
+            # Convert inputs to floats
+            gpt_val = float(usingGpt)
+            google_val = float(usingGoogle)
+            mind_val = float(usingMind)
+            input_data = np.array([gpt_val, google_val, mind_val]).reshape(1, -1)
+
+            # Perform prediction
+            total = smartness_prediction(input_data)
+        except ValueError as e:
+            st.error(f"Error: {e}. Please enter valid numerical values.")
+            total = "Please enter valid numerical values ."
+
+    st.success(total)
+    
+    #only when it is running from standalone file 
+if  __name__=='__main__':
+    main()
+    
