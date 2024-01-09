e9c441d041d327ec56ff6a2eca0fc0cb330dbe7e
Nihad El Bari <110766469+Niihaad@users.noreply.github.com>
2024-01-09 12:16:24 +0100

    Update Smartness_level_test.py

--- Smartness_level_test.py
+++ Smartness_level_test.py
@@ -1,39 +1,29 @@
-import numpy as np
-import pickle 
-import streamlit as st
 import requests
-from io import BytesIO
+import numpy as np
+import streamlit as st
+import io
+import joblib
 
-def load_model_from_github(model_url):
-    st.write("Fetching the model...")
-    response = requests.get(model_url)
-    if response.status_code == 200:
-        try:
-            loaded_model = pickle.load(BytesIO(response.content))
-            st.write("Model loaded successfully!")
-            return loaded_model
-        except Exception as e:
-            st.error(f"Error loading the model: {e}")
-            return None
-    else:
-        st.error("Failed to fetch the model")
-        return None
+# Function to load model from a URL
+def load_model_from_url(url):
+    response = requests.get(url)
+    model = joblib.load(io.BytesIO(response.content))
+    return model
 
-def smartness_prediction(input_data, model):
-    if model:
-        predicted_outcome = model.predict(input_data)
-        return f'Predicted smartness level: {predicted_outcome[0]}'
-    else:
-        return "Model not loaded properly"
+# Load the model from the GitHub URL
+model_url = 'https://github.com/Niihaad/ML_Algorithms/raw/main/trained_modelIntell.sav'
+loaded_model = load_model_from_url(model_url)
+
+def smartness_prediction(input_data):
+    predicted_outcome = loaded_model.predict(input_data)
+    return 'You are ' + str(predicted_outcome[0])
 
 def main():
-    st.title('Test Your Level of Smartness By Nihad')
-    model_url = 'https://github.com/Niihaad/ML_Algorithms/blob/main/trained_modelIntell.sav'
-    loaded_model = load_model_from_github(model_url)
+    st.title('Test Your Level of Smartness By Nihad ')
     
-    usingGpt = st.text_input('On a scale from 0 to 1, how frequently do you use ChatGPT?')
-    usingGoogle = st.text_input('On a scale from 0 to 1, how often do you rely on Google?')
-    usingMind = st.text_input('On a scale from 0 to 1, how frequently do you engage your mind?')
+    usingGpt = st.text_input('On a scale from 0 to 1, how frequently do you use ChatGPT ?')
+    usingGoogle = st.text_input('On a scale from 0 to 1, how often do you rely on Google ?')
+    usingMind = st.text_input('On a scale from 0 to 1, how frequently do you engage your mind ?')
     
     total = ''
     if st.button("Predict Smartness"):
@@ -42,13 +32,12 @@ def main():
             google_val = float(usingGoogle)
             mind_val = float(usingMind)
             input_data = np.array([gpt_val, google_val, mind_val]).reshape(1, -1)
-
-            total = smartness_prediction(input_data, loaded_model)
+            total = smartness_prediction(input_data)
         except ValueError as e:
             st.error(f"Error: {e}. Please enter valid numerical values.")
             total = "Please enter valid numerical values."
 
     st.success(total)
-
+    
 if __name__=='__main__':
     main()
