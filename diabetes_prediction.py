555381e854a44e55e307ca9fe0944024c3ba3a52
Nihad El Bari <110766469+Niihaad@users.noreply.github.com>
2024-01-09 03:49:42 +0100

    Update diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -9,21 +9,30 @@ import streamlit as st
 import requests
 from io import BytesIO
 
-# Download the model from GitHub
-model_url = 'https://github.com/Niihaad/ML_Algorithms/raw/main/trained_modelIntell.sav'
-response = requests.get(model_url)
+def load_model_from_github(model_url):
+    response = requests.get(model_url)
+    if response.status_code == 200:
+        try:
+            loaded_model = pickle.load(BytesIO(response.content))
+            return loaded_model
+        except Exception as e:
+            st.error(f"Error loading the model: {e}")
+            return None
+    else:
+        st.error("Failed to fetch the model")
+        return None
 
-# Load the model from the downloaded content
-if response.status_code == 200:
-    loaded_model = pickle.load(BytesIO(response.content))
-
-# Function for prediction
-def smartness_prediction(input_data):
-    predicted_outcome = loaded_model.predict(input_data)
-    return f'Predicted smartness level: {predicted_outcome[0]}'
+def smartness_prediction(input_data, model):
+    if model:
+        predicted_outcome = model.predict(input_data)
+        return f'Predicted smartness level: {predicted_outcome[0]}'
+    else:
+        return "Model not loaded properly"
 
 def main():
     st.title('Test Your Level of Smartness By Nihad')
+    model_url = 'https://github.com/Niihaad/ML_Algorithms/raw/main/trained_modelIntell.sav'
+    loaded_model = load_model_from_github(model_url)
     
     usingGpt = st.text_input('On a scale from 0 to 1, how frequently do you use ChatGPT?')
     usingGoogle = st.text_input('On a scale from 0 to 1, how often do you rely on Google?')
@@ -32,21 +41,17 @@ def main():
     total = ''
     if st.button("Predict Smartness"):
         try:
-            # Convert inputs to floats
             gpt_val = float(usingGpt)
             google_val = float(usingGoogle)
             mind_val = float(usingMind)
             input_data = np.array([gpt_val, google_val, mind_val]).reshape(1, -1)
 
-            # Perform prediction
-            total = smartness_prediction(input_data)
+            total = smartness_prediction(input_data, loaded_model)
         except ValueError as e:
             st.error(f"Error: {e}. Please enter valid numerical values.")
             total = "Please enter valid numerical values."
 
     st.success(total)
-    
+
 if __name__=='__main__':
     main()
-
-    
